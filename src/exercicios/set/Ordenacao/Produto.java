4ccab38e6d12a89749ae4e453a29dfb5ca5a6042
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-22 22:40:05 -0300

    feat: resolve exercicio 1 (sort-set)

---
+++ src/exercicios/set/Ordenacao/Produto.java
@@ -0,0 +1,72 @@
+package exercicios.set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+/**
+ * @author Pedro Lôbo Nascimento
+ */
+public class Produto implements Comparable<Produto> {
+    private String nome;
+    private long cod;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long cod, String nome, double preco, int quantidade) {
+        this.cod = cod;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public int compareTo(Produto o) {
+        return nome.compareToIgnoreCase(o.getNome());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getCod() == produto.getCod();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCod());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", cod=" + cod +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+}
