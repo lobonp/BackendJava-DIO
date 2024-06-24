b3af763c05498c33be863b10aa79f5ccb1923e18
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-24 19:38:38 -0300

    feat: importa implementacoes base do BancoDigital

---
+++ src/projetos/BancoDigital/IConta.java
@@ -0,0 +1,11 @@
+package projetos.BancoDigital;
+public interface IConta {
+	
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
+}
