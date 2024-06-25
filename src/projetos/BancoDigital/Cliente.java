b938ddad30383cd38359dce39a29f5e992d87ea8
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-25 16:01:13 -0300

    feat: cria atributo tipoPessoa e getter

--- src/projetos/BancoDigital/Cliente.java
+++ src/projetos/BancoDigital/Cliente.java
@@ -1,6 +1,8 @@
 package projetos.BancoDigital;
+
 public class Cliente {
 	private String nome;
+	private String tipoPessoa;
 
 	public String getNome() {
 		return nome;
@@ -10,4 +12,7 @@ public class Cliente {
 		this.nome = nome;
 	}
 
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
 }
