2ba7f9e614aedfaa0606a7faea59cf0202c44a14
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-25 16:21:25 -0300

    feat: acrescenta atributos score, cartaoCredito, milhas e faturaCartaoCreditoao Cliente e cria getters e setters

--- src/projetos/BancoDigital/Cliente.java
+++ src/projetos/BancoDigital/Cliente.java
@@ -3,6 +3,10 @@ package projetos.BancoDigital;
 public class Cliente {
 	private String nome;
 	private String tipoPessoa;
+	private int score; //score do cliente para liberar cartão de credito
+	private boolean cartaoCredito; //possui cartão de credito (necessario score >= 5)
+	private int milhas; //milhas do cliente acumuladas por usar cartão de credito
+	private double faturaCartaoCredito; //fatura do cartão de crédito
 
 	public String getNome() {
 		return nome;
@@ -23,5 +27,37 @@ public class Cliente {
 		} else {
 			return this.nome + " - Pessoa Jurídica";
 		}
+	}
+
+	public boolean isCartaoCredito() {
+		return cartaoCredito;
+	}
+
+	public void setCartaoCredito(boolean cartaoCredito) {
+		this.cartaoCredito = cartaoCredito;
+	}
+
+	public int getMilhas() {
+		return milhas;
+	}
+
+	public void setMilhas(int milhas) {
+		this.milhas = milhas;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public double getFaturaCartaoCredito() {
+		return faturaCartaoCredito;
+	}
+
+	public void setFaturaCartaoCredito(double faturaCartaoCredito) {
+		this.faturaCartaoCredito = faturaCartaoCredito;
 	}
 }
