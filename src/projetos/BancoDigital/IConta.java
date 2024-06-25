c653f4e9c8adef6f8c69b17fa8723b3346eef4a8
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-25 15:53:18 -0300

    feat: adiciona tipoTransferencia no metodo transferir()

--- src/projetos/BancoDigital/IConta.java
+++ src/projetos/BancoDigital/IConta.java
@@ -1,11 +1,12 @@
 package projetos.BancoDigital;
+
 public interface IConta {
 	
 	void sacar(double valor);
 	
 	void depositar(double valor);
 	
-	void transferir(double valor, IConta contaDestino);
+	void transferir(double valor, IConta contaDestino, String tipoTransferencia);
 	
 	void imprimirExtrato();
 }
