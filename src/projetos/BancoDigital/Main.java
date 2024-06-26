f73b9e88da2b4aa9587d30f3a6525e14ed473257
lobonp <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-06-26 00:08:15 -0300

    feat: incrementa os testes das funcionalidades

--- src/projetos/BancoDigital/Main.java
+++ src/projetos/BancoDigital/Main.java
@@ -1,19 +1,43 @@
 package projetos.BancoDigital;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Cliente pedro = new Cliente("Pedro", "Fisica");
+		Cliente kabum = new Cliente("Kabum", "Juridica");
+		Cliente microsoft = new Cliente("Microsoft", "Juridica");
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca, "ted");
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+		Conta pedro_cc = new ContaCorrente(pedro);
+		Conta pedro_poupanca = new ContaPoupanca(pedro);
+
+		pedro_cc.imprimirExtrato();
+		pedro_poupanca.imprimirExtrato();
+
+		Conta kabum_cc = new ContaCorrente(kabum);
+		kabum_cc.imprimirExtrato();
+
+		Conta microsoft_cc = new ContaCorrente(microsoft);
+		microsoft_cc.imprimirExtrato();
+
+		pedro_cc.depositar(1000);
+		kabum_cc.depositar(100);
+		microsoft_cc.depositar(50000);
+
+		pedro_cc.transferir(100, pedro_poupanca, "TED");
+		pedro_cc.transferir(10, kabum_cc, "pix");
+		pedro_cc.transferir(10, kabum_cc, "pix");
+		pedro_cc.transferir(10, kabum_cc, "pix");
+		pedro_cc.transferir(10, kabum_cc, "pix");
+
+		//simulando pagamento de salario por TED
+		microsoft_cc.transferir(1800, pedro_cc, "TED");
+
+		//simulando compra com cartao de credito em loja
+		pedro_cc.transferir(500, kabum_cc, "credito");
+
+		System.out.println(pedro.getScore());
 	}
 
 }
